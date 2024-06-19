cba0aeef0cb7a9992a54cccf85f660eaed2540a6
sofiacharnota <sofiacharnota@gmail.com>
2024-06-19 16:02:52 +0200

    tornado test

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import os
-import time
-
-# # Set environment variables before importing FiftyOne
-# os.environ["FIFTYONE_DATABASE_DIR"] = "/tmp/db/"
-# os.environ["FIFTYONE_DEFAULT_DATASET_DIR"] = "/tmp/db/"
-# os.environ["FIFTYONE_DATASET_ZOO_DIR"] = "/tmp/db_zoo/"
-# os.environ["FIFTYONE_MODEL_ZOO_DIR"] = "/tmp/zoo_model/"
-
+import tornado.ioloop
+import tornado.web
 import fiftyone as fo
 import fiftyone.zoo as foz
-from fiftyone import ViewField as F
 
-# Ensure FiftyOne reads the configurations from the environment variables
-fo.config.database_dir = os.environ["FIFTYONE_DATABASE_DIR"]
-fo.config.default_dataset_dir = os.environ["FIFTYONE_DEFAULT_DATASET_DIR"]
-fo.config.dataset_zoo_dir = os.environ["FIFTYONE_DATASET_ZOO_DIR"]
-fo.config.model_zoo_dir = os.environ["FIFTYONE_MODEL_ZOO_DIR"]
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("FiftyOne App is running")
 
-# Load dataset
-dataset = foz.load_zoo_dataset("quickstart")
+class FiftyOneHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
-# Launch the FiftyOne app
-session = fo.launch_app(dataset, address='0.0.0.0', port=5151, remote=True)
+def make_app():
+    # Define the URL prefix
+    url_prefix = os.getenv("URL_PREFIX", "/")
 
-# Show the session
-session.show()
+    return tornado.web.Application([
+        (url_prefix, MainHandler),
+        (url_prefix + "fiftyone", FiftyOneHandler),
+    ])
 
-# Update the session view
-session.view = (
-    dataset
-    .sort_by("uniqueness", reverse=True)
-    .limit(25)
-    .filter_labels("predictions", F("confidence") > 0.5)
-)
-while True:
-    time.sleep(10)
+if __name__ == "__main__":
+    # Load dataset
+    dataset = foz.load_zoo_dataset("quickstart")
+
+    # Launch the FiftyOne app
+    session = fo.launch_app(dataset, address='0.0.0.0', port=5151, remote=True)
+
+    # Show the session
+    session.show()
+
+    # Update the session view
+    session.view = (
+        dataset
+        .sort_by("uniqueness", reverse=True)
+        .limit(25)
+        .filter_labels("predictions", F("confidence") > 0.5)
+    )
+
+    # Start the Tornado server
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
